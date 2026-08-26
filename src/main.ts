27f1d5f045212c6942bda28050d3e93d52904e54
Célio Bastian <celio3astian@gmail.com>
2026-08-26 09:50:14 -0300

    feat: implementa busca na pokeapi

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-console.log("Pokédex Typescript Lite");
+import { buscarPokemon } from "./services/PokeApiService.js";
+
+async function main() {
+    const pokemon = await buscarPokemon("pikachu");
+    
+    if (pokemon !== null) {
+        console.log(pokemon);
+    
+    }
+}    
+    main();
---
+++ src/services/PokeApiService.ts
@@ -0,0 +1,35 @@
+import type {PokemonApiResponse, PokemonResumo} from "../models/Pokemon.js";
+
+export async function buscarPokemon(
+    nomeOuId: string
+): Promise<PokemonResumo | null>{
+
+
+    try{
+        const url = `https://pokeapi.co/api/v2/pokemon/${nomeOuId}`;
+
+        const resposta = await fetch(url);
+
+        if (!resposta.ok){
+            console.log(`[ERRO] Pokémon não encontrado:${nomeOuId}`);
+            return null;
+        }
+
+        const dados = await resposta.json() as PokemonApiResponse;
+
+        const pokemon: PokemonResumo = {
+            id: dados.id,
+            nome: dados.name,
+            tipos: dados.types.map((item) => item.type.name),
+            altura: dados.height,
+            peso: dados.weight
+        };
+
+        console.log (`[OK] Pokemon encontrado:${pokemon.nome}`);
+        return pokemon;
+
+    }catch (erro){
+        console.log('[ERRO] Não foi possível buscar o Pokémon.');
+        return null;
+    }
+}
